47b24e71b7c5fa849c5338a567bd051ff9d0f6a1
John Scott <john.c.scott@microsoft.com>
2025-07-24 13:47:17 -0700

    Update deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
   azure: {
-    storageAccountName: 'stparty54329',
-    sasToken: 'sv=2022-11-02&ss=t&srt=sco&sp=rwdlacupi&se=2025-01-26T03:40:29Z&st=2025-01-25T19:40:29Z&spr=https&sig=SHdKxUV4CjzNB6P2jLnfUZ7wCWgagJ1mC6pbYpP0F8Q%3D',
-    tableName: 'rsvp',
-    commentTableName: 'comments'
+    storageAccountName: (globalThis as any)?.process?.env?.['AZURE_STORAGE_ACCOUNT_NAME'] || 'stparty54329',
+    sasToken: (globalThis as any)?.process?.env?.['AZURE_STORAGE_SAS_TOKEN'] || 'sv=2022-11-02&ss=t&srt=sco&sp=rwdlacupi&se=2025-01-26T03:40:29Z&st=2025-01-25T19:40:29Z&spr=https&sig=SHdKxUV4CjzNB6P2jLnfUZ7wCWgagJ1mC6pbYpP0F8Q%3D',
+    tableName: (globalThis as any)?.process?.env?.['AZURE_STORAGE_TABLE_NAME'] || 'rsvp',
+    commentTableName: (globalThis as any)?.process?.env?.['AZURE_STORAGE_COMMENT_TABLE_NAME'] || 'comments'
   }
 };
